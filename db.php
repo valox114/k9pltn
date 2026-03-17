d996a241cd51283579576faa0a539759bbf14c44
HGammoudi <h.gamma.gammoudi@gmail.com>
2026-03-17 05:23:28 +0100

    v0.3

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "k9pltn";
+$host = "sql313.infinityfree.com";       // InfinityFree MySQL host
+$user = "if0_41410053";                  // Your MySQL username
+$pass = "YOUR_DB_PASSWORD";              // The password you set when creating the DB
+$db   = "if0_41410053_k9pltn";          // Your MySQL database name
 
 $conn = new mysqli($host, $user, $pass, $db);
 
